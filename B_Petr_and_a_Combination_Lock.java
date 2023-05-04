e3624ba21a0537e4eb8eabf606e1edfc29eb088c
2021kuec2057@iiitkota.ac.in <2021kuec2057@iiitkota.ac.in>
2023-05-04 10:54:07 +0530

    more practice

---
+++ B_Petr_and_a_Combination_Lock.java
@@ -0,0 +1,164 @@
+/**
+ * B_Petr_and_a_Combination_Lock
+ */
+import java.util.*;
+import java.io.*;
+public class B_Petr_and_a_Combination_Lock {
+
+    //My code is my Identity
+    public static void main(String[] args) 
+    {
+    FastScanner sc = new FastScanner(System.in);
+    PrintWriter pr = new PrintWriter(System.out);
+    int temp2=sc.nextInt();
+    String res="No";  
+    int arr[]=new int[temp2];
+    for(int i=0;i<temp2;i++)
+    arr[i]=sc.nextInt();
+    long temp_sum=0;
+    first:
+    for(int i=0;i<=(1<<temp2)-1;i++){
+        int temp1=i;   
+        for(int j=0;j<temp2;j++){
+          int rem=temp1%2;
+          temp1/=2;
+        //   pr.println("rem :"+rem);
+          if(rem==0)
+          temp_sum+=arr[j];
+          else
+          temp_sum-=arr[j];
+        }
+        if(temp_sum%360==0)
+        {
+            // pr.println(i+" "+temp_sum);
+            res="Yes";
+            break first;
+        }
+        temp_sum=0;
+    }
+    pr.println(res);
+    pr.close();
+    }
+    static long gcd(long a, long b)
+    {
+    if (b == 0)
+      return a;
+    else
+    return gcd(b,a%b);
+    }
+    static long andOperator(long a, long b)
+    {
+    int shiftcount = 0;
+    while (a != b && a > 0) {
+    shiftcount++;
+    a = a >> 1;
+    b = b >> 1;
+    }
+    return (a << shiftcount);
+    }
+}
+
+
+class ArrayOfPairsSorter {
+static void sort(Pair[] arr) {
+Comparator<Pair> comparator = new Comparator<>() {
+@Override
+public int compare(Pair p1, Pair p2) {
+return p2.y
+ - p1.y; // To compare the first element
+        // just
+        // change the variable from p1.y
+        // - p2.y to p1.x-p2.x.
+}
+};
+Arrays.sort(arr, comparator);
+}
+}
+class Pair {
+int x;
+int y;
+// Constructor
+public Pair(int x, int y) {
+    this.x = x;
+    this.y = y;
+}
+public int getX(){
+    return x;
+}
+public int getY(){
+    return y;
+}
+  // Overriding toString method
+//       for beautiful printing of pairs
+@Override
+public String toString() {
+    return "("+x+","+y+")" ;
+}
+}
+class FastScanner 
+{
+private BufferedReader reader = null;
+private StringTokenizer tokenizer = null;
+public FastScanner(InputStream in)
+{
+reader = new BufferedReader(new InputStreamReader(in));
+tokenizer = null;
+}
+public String next() 
+{
+if (tokenizer == null || !tokenizer.hasMoreTokens()) 
+{
+try 
+{
+tokenizer = new StringTokenizer(reader.readLine());
+} 
+catch (IOException e)
+{
+throw new RuntimeException(e);
+}
+}
+return tokenizer.nextToken();
+}
+public String nextLine()
+{
+if (tokenizer == null || !tokenizer.hasMoreTokens())
+{
+try
+{
+return reader.readLine();
+} 
+catch (IOException e)
+{
+throw new RuntimeException(e);
+}
+}
+return tokenizer.nextToken(
+);
+}
+public long nextLong() 
+{
+ return Long.parseLong(next());
+}
+public int nextInt()
+{
+return Integer.parseInt(next());
+}
+public double nextDouble() 
+{
+return Double.parseDouble(next());
+}
+public int[] nextIntArray(int n)
+{
+int[] a = new int[n];
+for (int i = 0; i < n; i++)
+a[i] = nextInt();
+return a;
+}
+public long[] nextLongArray(int n)
+{
+long[] a = new long[n];
+for (int i = 0; i < n; i++)
+a[i] = nextLong();
+return a;
+} 
+}
